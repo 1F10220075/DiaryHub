e498b85603ed82fc3d92d9f9b2429c8efacbd7c7
1F10220075 <107395473+1F10220075@users.noreply.github.com>
2024-01-04 23:08:34 +0900

    Djangoの復習2
    channelsライブラリの結合

--- diaryhub/views.py
+++ diaryhub/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "diaryhub/index.html")
+
+def diary(request, diary_name):
+    return render(request, "diaryhub/diary.html", {"diary_name": diary_name})
