c32ba5a85276dbf1bc4f4578f8a002e1ee1777dc
1F10220075 <107395473+1F10220075@users.noreply.github.com>
2024-01-07 01:43:24 +0900

    Add redirect for /8000/ to main page
    Added functionality to redirect to the main page when accessing the /8000/ URL. This ensures a smoother user experience and simplifies navigation.

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('diary/', include("diaryhub.urls")),
     path('admin/', admin.site.urls),
     path('app01/', include('authtest.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='app01'))
 ]
