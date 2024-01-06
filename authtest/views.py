5123a562c7868f4f5c33f1f0ffd7403af201944a
1F10220075 <107395473+1F10220075@users.noreply.github.com>
2024-01-07 01:06:06 +0900

    Djangoの復習5
    認証が必要なページと認証不要なページの作成

--- authtest/views.py
+++ authtest/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
-    return render(request, 'authtest/home.html',{})
+    return render(request, 'authtest/home.html', {})
+
+@login_required # 関数やクラス前につけることでログインを必須にできる
+def private_page(request):
+    return render(request, 'authtest/private.html', {})
+
+def public_page(request):
+    return render(request, 'authtest/public.html', {})
