67c2c2cb267f37cd8006c566f778e6aca443b3b0
1F10220075 <107395473+1F10220075@users.noreply.github.com>
2024-01-07 23:49:08 +0900

    日記の一覧の表示
    index.htmlにすべての日記が表示されえるようにした

--- diaryhub/views.py
+++ diaryhub/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Diary
 
 # Create your views here.
 def index(request):
-    return render(request, "diaryhub/index.html")
+    diarys = Diary.objects.all()
+    context = {
+        'diarys':diarys,
+    }
+    return render(request, "diaryhub/index.html", context)
 
 def diary(request, diary_name):
     return render(request, "diaryhub/diary.html", {"diary_name": diary_name})
