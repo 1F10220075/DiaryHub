5df125245cf228886a283bdf0ca5db72b59cd8bc
1F10220075 <107395473+1F10220075@users.noreply.github.com>
2024-01-10 10:49:39 +0900

    日記の検索機能

--- diaryhub/views.py
+++ diaryhub/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from .models import Diary
+from .forms import SearchForm
 
 # Create your views here.
 def index(request):
-    return render(request, "diaryhub/index.html")
+    searchForm = SearchForm(request.GET)
+    if searchForm.is_valid():
+        keyword = searchForm.cleaned_data['keyword']
+        diarys = Diary.objects.filter(content__contains=keyword)
+    else:
+        searchForm = SearchForm()
+        diarys = Diary.objects.all()
+
+    context = {
+        'message': 'Hello Django',
+        'diarys': diarys,
+        'searchForm': searchForm,
+    }
+    return render(request, 'bbs/index.html', context)
 
 def diary(request, diary_name):
     return render(request, "diaryhub/diary.html", {"diary_name": diary_name})
