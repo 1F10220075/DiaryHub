d8b8ccf6dd8b6995bb49e0feb8f73faace998f64
1F10220075 <107395473+1F10220075@users.noreply.github.com>
2024-01-10 18:40:07 +0900

    DiaryModelの再定義

--- diaryhub/models.py
+++ diaryhub/models.py
@@ -1,9 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 class Diary(models.Model):
-    content = models.CharField(max_length=200)
-    user_name = models.CharField(max_length=200, null = True)
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
