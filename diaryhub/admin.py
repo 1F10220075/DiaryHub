f06347c12865f760a5c322bbb8278348a7ff3d6b
1F10220075 <107395473+1F10220075@users.noreply.github.com>
2024-01-10 18:56:04 +0900

    管理サイトを設定

--- diaryhub/admin.py
+++ diaryhub/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Diary
+from .models import Diary, Category
 
 # Register your models here.
+admin.site.register(Category)
 admin.site.register(Diary)
